d85e3677eb4918acb4d4e2c16ee4efb6ff8b1dff
James Cool <jeffreysbrother@gmail.com>
2016-10-01 21:42:24 -0700

    update package.json and add fs to renderer

--- renderer.js
+++ renderer.js
@@ -1,3 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+document.write("<hr><p>This text is coming from renderer.js<p><hr>");
+
+var fs = require('fs');
+var contents = fs.readFileSync('./package.json', 'utf8');
+alert(contents);
