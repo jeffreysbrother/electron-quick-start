3301fdf4fd2fcad97cbe2adb2610ba1152855699
James Cool <jeffreysbrother@gmail.com>
2016-10-01 22:12:36 -0700

    remove files...not working correctly

--- MyApp-darwin-x64/MyApp.app/Contents/Resources/app/renderer.js
+++
@@ -1,8 +0,0 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-document.write("<hr><p>This text is coming from renderer.js<p><hr>");
-
-var fs = require('fs');
-var contents = fs.readFileSync('./package.json', 'utf8');
-alert(contents);
